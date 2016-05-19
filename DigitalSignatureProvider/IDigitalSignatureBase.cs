9b5ec88e08007be600503f67d11adb30d9328061
gekakud <geka.kud@gmail.com>
2016-05-19 16:59:23 +0300

    digital signature stuff

---
+++ DigitalSignatureProvider/IDigitalSignatureBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignatureProvider
+{
+    interface IDigitalSignatureBase
+    {
+        bool SignDocument(string p_docPath, string p_algType);
+
+    }
+}
