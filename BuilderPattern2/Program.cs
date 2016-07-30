5c2fb4314fd83db40bf1510040a3b30266f90494
gekakud <geka.kud@gmail.com>
2016-07-30 15:42:28 +0300

    BuilderPattern2.csproj

---
+++ BuilderPattern2/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace BuilderPattern2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pizza p = new PizzaBuilder().AddCheese().AddOlives().SetPizzaSize();
+        }
+    }
+
+    public class PizzaBuilder
+    {
+        private readonly Pizza _pizzaInProduction = new Pizza();
+
+        public Pizza Build()
+        {
+            return new Pizza
+            {
+                Cheese = _pizzaInProduction.Cheese,
+                XLsize = _pizzaInProduction.XLsize,
+                ExtraCheese = _pizzaInProduction.ExtraCheese,
+                Olives = _pizzaInProduction.Olives,
+                Onions = _pizzaInProduction.Onions
+            };
+        }
+
+        public static implicit operator Pizza(PizzaBuilder p_builder)
+        {
+            return p_builder.Build();
+        }
+
+        public PizzaBuilder AddCheese()
+        {
+            _pizzaInProduction.Cheese = true;
+            return this;
+        }
+
+        public PizzaBuilder AddOnion()
+        {
+            _pizzaInProduction.Onions = true;
+            return this;
+        }
+
+        public PizzaBuilder AddOlives()
+        {
+            _pizzaInProduction.Olives = true;
+            return this;
+        }
+
+        public PizzaBuilder SetPizzaSize()
+        {
+            _pizzaInProduction.XLsize = true;
+            return this;
+        }
+
+        public PizzaBuilder SetDoubleCheese()
+        {
+            _pizzaInProduction.ExtraCheese = true;
+            return this;
+        }
+    }
+
+    public class Pizza
+    {
+        private double _cost;
+
+        public Pizza()
+        {
+            _cost = 0;
+        }
+
+        public bool Onions { get; set; }
+        public bool Olives { get; set; }
+        public bool Cheese { get; set; }
+        public bool XLsize { get; set; }
+        public bool ExtraCheese { get; set; }
+
+        public double GetPizzaCost()
+        {
+            return _cost;
+        }
+
+        public void SetDiscount(double p_discount)
+        {
+            _cost = _cost*p_discount;
+        }
+    }
+}
--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -11,7 +11,7 @@ namespace Reflection
         {
             var assembly =
                 Assembly.LoadFile(
-                    @"C:\Users\EvgenyK\Desktop\dotnettutors\CommonUtilsLibrary\bin\Debug\CommonUtilsLibrary.dll");
+                    @"C:\Users\gekak_000\Desktop\MyTutorial\CommonUtilsLibrary\bin\Debug\CommonUtilsLibrary.dll");
             var myType = assembly.GetType("CommonUtilsLibrary.CommonUtils");
 
             IList<Type> listOfTypes = assembly.GetExportedTypes().ToList();
@@ -20,90 +20,7 @@ namespace Reflection
 
             var instance = Activator.CreateInstance(myType);
 
-            Pizza p = new PizzaBuilder().AddCheese().AddOlives().SetPizzaSize();
-
-
             Console.ReadKey();
-        }
-    }
-
-    public class PizzaBuilder
-    {
-        private readonly Pizza _pizzaInProduction = new Pizza();
-
-        public Pizza Build()
-        {
-            return new Pizza
-            {
-                Cheese = _pizzaInProduction.Cheese,
-                XLsize = _pizzaInProduction.XLsize,
-                ExtraCheese = _pizzaInProduction.ExtraCheese,
-                Olives = _pizzaInProduction.Olives,
-                Onions = _pizzaInProduction.Onions
-            };
-        }
-
-        public PizzaBuilder AddCheese()
-        {
-            _pizzaInProduction.Cheese = true;
-            //WTF?
-            _pizzaInProduction.Cheese = false;
-            return this;
-        }
-
-        public PizzaBuilder AddOnion()
-        {
-            _pizzaInProduction.Onions = true;
-            return this;
-        }
-
-        public PizzaBuilder AddOlives()
-        {
-            _pizzaInProduction.Olives = true;
-            return this;
-        }
-
-        public static implicit operator Pizza(PizzaBuilder p_builder)
-        {
-            return p_builder.Build();
-        }
-
-        public PizzaBuilder SetPizzaSize()
-        {
-            _pizzaInProduction.XLsize = true;
-            return this;
-        }
-
-        public PizzaBuilder SetDoubleCheese()
-        {
-            _pizzaInProduction.ExtraCheese = true;
-            return this;
-        }
-    }
-
-    public class Pizza
-    {
-        private double _cost;
-
-        public Pizza()
-        {
-            _cost = 0;
-        }
-
-        public bool Onions { get; set; }
-        public bool Olives { get; set; }
-        public bool Cheese { get; set; }
-        public bool XLsize { get; set; }
-        public bool ExtraCheese { get; set; }
-
-        public double GetPizzaCost()
-        {
-            return _cost;
-        }
-
-        public void SetDiscount(double p_discount)
-        {
-            _cost = _cost*p_discount;
         }
     }
 }
