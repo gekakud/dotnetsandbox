40b18a1aaaf8cf96f89c494d383b6676e2d48c65
gekakud <geka.kud@gmail.com>
2016-08-12 16:06:41 +0300

    TPLDataflow

--- TPLDataflow/Program.cs
+++ TPLDataflow/Program.cs
@@ -1,46 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks.Dataflow;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace TPLDataflow
 {
     public class Program
     {
+        private Timer _t;
         private static void Main()
-        {
+        {         
+            DataflowExecutor d = new DataflowExecutor(new Configs{BufferSize = 2,UIBufferSize = 3,DelayTime = 2});
+
+            var i = 0;
+            while (true)
+            {
+                d.PutDataIntoPipe(i);
+                i++;
+            Thread.Sleep(500);
+            }
         }
     }
 
     public class DataflowExecutor
     {
-        private readonly BatchBlock<int> _bufferBatchBlock;
-        private readonly TransformBlock<IEnumerable<int>, string> _buildTransformBlock;
-        private readonly BatchBlock<string> _dataStoreBatchBlock;
+        private BatchBlock<int> _bufferBatchBlock;
+        private  TransformBlock<IEnumerable<int>, string> _buildTransformBlock;
+        private  BatchBlock<string> _dataStoreBatchBlock;
         private BatchBlock<string> _itemsToUpdateUI;
-        private readonly TransformBlock<IEnumerable<int>, IEnumerable<int>> _mergeTransformBlock;
+        private  TransformBlock<IEnumerable<int>, IEnumerable<int>> _mergeTransformBlock;
+        public int UpdateTime = 1;
+        private Timer _timer;
+
+        public void PutDataIntoPipe(int p_d)
+        {
+            _bufferBatchBlock.Post(p_d);
+        }
+
+        private bool CreateAndLinkBlocks(Configs p_configs)
+        {
+            try
+            {
+                _bufferBatchBlock = new BatchBlock<int>(p_configs.BufferSize);
+
+                _mergeTransformBlock =
+                    new TransformBlock<IEnumerable<int>, IEnumerable<int>>(
+                        (Func<IEnumerable<int>, IEnumerable<int>>)MergeStuff);
+                _buildTransformBlock =
+                    new TransformBlock<IEnumerable<int>, string>((Func<IEnumerable<int>, string>)BuildStuff);
+
+                _dataStoreBatchBlock = new BatchBlock<string>(p_configs.BufferSize);
+                _itemsToUpdateUI = new BatchBlock<string>(p_configs.UIBufferSize);
+
+                _bufferBatchBlock.LinkTo(_mergeTransformBlock);
+                _mergeTransformBlock.LinkTo(_buildTransformBlock);
+
+                //CON
+                var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+                _buildTransformBlock.LinkTo(_dataStoreBatchBlock, linkOptions, p_p => p_p.Length > 5);
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
 
         public DataflowExecutor(Configs p_configs)
         {
-            _bufferBatchBlock = new BatchBlock<int>(p_configs.BufferSize);
-
-            _mergeTransformBlock =
-                new TransformBlock<IEnumerable<int>, IEnumerable<int>>(
-                    (Func<IEnumerable<int>, IEnumerable<int>>) MergeStuff);
-            _buildTransformBlock =
-                new TransformBlock<IEnumerable<int>, string>((Func<IEnumerable<int>, string>) BuildStuff);
-
-            _dataStoreBatchBlock = new BatchBlock<string>(p_configs.BufferSize);
-            _itemsToUpdateUI = new BatchBlock<string>(p_configs.UIBufferSize);
-
-            _bufferBatchBlock.LinkTo(_mergeTransformBlock);
-            _mergeTransformBlock.LinkTo(_buildTransformBlock);
-
-            //CON
-            var linkOptions = new DataflowLinkOptions {PropagateCompletion = true};
-            _buildTransformBlock.LinkTo(_dataStoreBatchBlock, linkOptions, p_p => p_p.Length > 5);
+            if (CreateAndLinkBlocks(p_configs))
+            {
+                SetTimeOutForBuffer(p_configs.DelayTime);
+            }
         }
 
+        private void SetTimeOutForBuffer(int p_delayTime)
+        {
+            UpdateTime = p_delayTime;
+            _timer = new Timer(UpdateTime*1000){AutoReset = true};
+            _timer.Elapsed += (x, y) => { _bufferBatchBlock.TriggerBatch(); };
+            _timer.Start();
+        }
 
         private IEnumerable<int> MergeStuff(IEnumerable<int> p_data)
         {
