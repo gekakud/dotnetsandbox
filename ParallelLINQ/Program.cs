b54c6c724ed0734a52530d25cc8f5f6f960a2639
gekakud <geka.kud@gmail.com>
2016-08-27 22:40:40 +0300

    ParallelLINQ.csproj

---
+++ ParallelLINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ParallelLINQ
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var numbers = new int[100000];
+            
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+
+            numbers[1000] = -5;
+            numbers[14000] = -2;
+            numbers[35000] = -4;
+            numbers[76000] = -3;
+            numbers[96000] = -1;
+            numbers[55000] = -6;
+
+            //find negative number in array and order by desc.          
+            var negatives = numbers.AsParallel().AsOrdered().Where(p_n => p_n < 0);           
+           
+            foreach (var negative in negatives)
+            {
+                Console.Write(negative + "  ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
