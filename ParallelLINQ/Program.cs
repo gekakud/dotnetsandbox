66f416903e3c2c592034602c02f81ec5b6a10787
gekakud <geka.kud@gmail.com>
2016-08-27 22:46:49 +0300

    linq ParallelLINQ/Program.cs

--- ParallelLINQ/Program.cs
+++ ParallelLINQ/Program.cs
@@ -22,9 +22,21 @@ namespace ParallelLINQ
             numbers[55000] = -6;
 
             //find negative number in array and order by desc.          
-            var negatives = numbers.AsParallel().AsOrdered().Where(p_n => p_n < 0);           
-           
+            var negatives = numbers.AsParallel().AsOrdered().Where(p_n => p_n < 0);
             foreach (var negative in negatives)
+            {
+                Console.Write(negative + "  ");
+            }
+            Console.WriteLine();
+            
+            //same thing
+            ParallelQuery<int> negs = from num in numbers
+                .AsParallel()
+                .AsOrdered()
+                where num < 0
+                select num;
+
+            foreach (var negative in negs)
             {
                 Console.Write(negative + "  ");
             }
