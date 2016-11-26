76a93febb612412e28bc4aa3545772ef0ce3e98f
gekakud <geka.kud@gmail.com>
2016-11-26 22:21:02 +0200

    linq

--- ParallelLINQ/Program.cs
+++ ParallelLINQ/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
 
 namespace ParallelLINQ
 {
@@ -21,27 +26,48 @@ namespace ParallelLINQ
             numbers[96000] = -1;
             numbers[55000] = -6;
 
-            //find negative number in array and order by desc.          
-            var negatives = numbers.AsParallel().AsOrdered().Where(p_n => p_n < 0);
-            foreach (var negative in negatives)
+            //find negative number in array using ParallelQuery ?
+            var watch = Stopwatch.StartNew();
+//            ParallelQuery<int> negatives = numbers.AsParallel().Where(p_n => p_n < 0);
+            //OR
+//            ParallelQuery<int> negs = from num in numbers
+//                .AsParallel()
+//                .AsOrdered()
+//                                        where num < 0
+//                                        select num;
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs);
+            watch.Reset();
+            ShowResult(negatives);
+            
+            //same thing but using Parallel for each loop ?
+//            var bag = new ConcurrentBag<int>();
+//            watch.Start();
+//
+//            Parallel.ForEach(numbers, num =>
+//            {
+//                if (num < 0)
+//                {
+//                    bag.Add(num);
+//                }
+//            });
+            
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs);
+            
+            ShowResult(bag.ToArray());
+
+            Console.ReadKey();
+        }
+
+        private static void ShowResult(IEnumerable<int> p_negatives)
+        {
+            foreach (var negative in p_negatives)
             {
                 Console.Write(negative + "  ");
             }
             Console.WriteLine();
-            
-            //same thing
-            ParallelQuery<int> negs = from num in numbers
-                .AsParallel()
-                .AsOrdered()
-                where num < 0
-                select num;
-
-            foreach (var negative in negs)
-            {
-                Console.Write(negative + "  ");
-            }
-
-            Console.ReadKey();
         }
     }
 }
