5a076ea89c928701ccabbf748dc85ecb7413a3bd
Evgeny Pekerman <geka.kud@gmail.com>
2019-06-19 12:28:42 +0300

    async await example

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Example();
+
+            Console.ReadKey();
+        }
+
+        static async void Example()
+        {
+            Executer e = new Executer();
+
+            bool t = await e.StartAsyncTask();
+
+            Console.WriteLine("HAHA");
+        }
+    }
+
+    public class Executer
+    {
+        public async Task<bool> StartAsyncTask()
+        {
+            try
+            {
+                await Task.Run(() => { Console.WriteLine("First await action"); })
+                        .ContinueWith(prevTask =>
+                        {
+                            Thread.Sleep(3000);
+                            Console.WriteLine("Second await action with prev task status:" + prevTask.Status);
+                        });
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("GOT exceptio:" + e.Message);
+            }
+
+            return true;
+        }
+
+        public async Task<bool> StartAsyncTaskWithException()
+        {
+            try
+            {
+                await Task.Run(() => { Console.WriteLine("First await action"); })
+                    .ContinueWith(prevTask =>
+                    {
+                        throw new Exception("hhh");
+                        Thread.Sleep(3000);
+                        Console.WriteLine("Second await action with prev task status:" + prevTask.Status);
+                    });
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("GOT exceptio:" + e.Message);
+            }
+
+            return true;
+        }
+    }
+}
