ca3c0f21bd97c72b3b8b8bf0f06637eba78a6d79
geka <geka.kud@gmail.com>
2019-11-26 15:13:34 +0200

    job queue impl

---
+++ ProdConsJobQueuePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ProdConsJobQueuePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientsProcessor = new QueueProcessor(5);
+
+            //adding new person(supermarket client) to a queue each second
+            var simpleDataStream = Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x =>
+            {
+                Console.WriteLine("Client {0} joined the queue", x);
+                clientsProcessor.AddClientToQueue((int)x);
+            });
+
+            Console.ReadKey();
+            simpleDataStream.Dispose();
+            clientsProcessor.Dispose();
+        }
+    }
+}
---
+++ ProdConsJobQueuePattern/QueueProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProdConsJobQueuePattern
+{
+    /// <summary>
+    /// Managing cashier threads and thread-safe queue
+    /// </summary>
+    public class QueueProcessor : IDisposable
+    {
+        //using BlockingCollection to avoid polling
+        private readonly BlockingCollection<int>
+            _clientsQueue = new BlockingCollection<int>(new ConcurrentQueue<int>());
+
+        //create independent cashiers(threads)
+        public QueueProcessor(int numThreads)
+        {
+            for (var i = 0; i < numThreads; i++)
+            {
+                var thread = new Thread(OnHandlerStart)
+                    { IsBackground = true };
+                thread.Start();
+            }
+        }
+
+        public void AddClientToQueue(int job)
+        {
+            _clientsQueue.Add(job);
+        }
+
+        private async void OnHandlerStart()
+        {
+            var rn = new Random(DateTime.Now.Millisecond);
+
+            while (true)
+            {
+                //blocking thread until there is new client to process
+                var currentClienId = _clientsQueue.Take();
+                var processingDelay = rn.Next(1, 5);
+
+                //processing...
+
+                await Task.Delay(processingDelay * 1000);
+
+                Console.WriteLine("Cashier {0} finished processing client {1} within {2} seconds"
+                    , Thread.CurrentThread.ManagedThreadId
+                    , currentClienId, processingDelay);
+            }
+        }
+
+        public void Dispose()
+        {
+            _clientsQueue?.Dispose();
+        }
+    }
+}
