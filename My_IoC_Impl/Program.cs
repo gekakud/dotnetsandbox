54e3ee9b98a0294e592be942eb9c4f71af15b66e
geka <geka.kud@gmail.com>
2019-12-06 08:29:47 +0200

    ioc container by reflection

---
+++ My_IoC_Impl/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_IoC_Impl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IoC.Register<ISomeService,SomeServiceImplementation>(new SomeServiceImplementation());
+            Console.WriteLine("Trying to resolve type...");
+            ISomeService someImpl = IoC.Resolve<ISomeService>();
+            someImpl.PrintGreeting();
+            Console.ReadKey();
+        }
+    }
+
+    interface ISomeService
+    {
+        void PrintGreeting();
+    }
+
+    class SomeServiceImplementation:ISomeService
+    {
+        public SomeServiceImplementation()
+        {
+            Console.WriteLine("Hello from {0} constructor!", this.GetType());
+        }
+
+        public void PrintGreeting()
+        {
+            Console.WriteLine("Hello there!");
+        }
+    }
+}
