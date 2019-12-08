e6e8dd2deb7b166e928f815d4dc4398d2dbd0fa3
Evgeny Pekerman <geka.kud@gmail.com>
2019-12-08 12:12:50 +0200

    GC added

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        //The finalizer method is called when your object is garbage collected and you have no guarantee when this will happen(you can force it, but it will hurt performance).
+
+        //The Dispose method on the other hand is meant to be called by the code that created your class so that you can clean up
+        //and release any resources you have acquired(unmanaged data, database connections, file handles, etc) the moment the code is done with your object.
+
+        //The standard practice is to implement IDisposable and Dispose so that you can use your object in a using statment. Such as using(var foo = new MyObject()) { }.
+        //And in your finalizer, you call Dispose, just in case the calling code forgot to dispose of you.
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
