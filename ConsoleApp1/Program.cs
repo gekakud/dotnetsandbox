df7904f20ccd4cf0294425397fbf9e7f527c50db
Evgeny Pekerman <evgeny.pekerman@IL-EVGENYP>
2020-05-14 20:05:46 +0300

    task progress + move to .net 4.7.2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check c = new Check();
+            c.check();
+
+            for (int i = 0; i < 2; i++,SleepHere())
+            {
+                var u = 6;
+            }
+
+            var t = 6;
+        }
+
+        static void SleepHere()
+        {
+            var y = 7;
+        }
+    }
+
+    internal class Check
+    {
+        public void check(CancellationToken token = default)
+        {
+            var t = 6;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            //running on threadpool threads
+            IProgress<string> progress = new Progress<string>(str =>
+            {
+                Console.WriteLine(
+                    $"Progress handled by TID: {Thread.CurrentThread.ManagedThreadId}::progress message=> {str}");
+                Console.Out.Flush();
+            });
+
+            var tasks = new Task[5];
+            for (var i = 0; i < 5; i++)
+            {
+                var j = i;
+                var t = Task.Run(async () =>
+                {
+                    await Task.Delay(500);
+                    progress.Report($"started! ThreadID: {Thread.CurrentThread.ManagedThreadId}\tNum: {j + 1}");
+
+                    await Task.Delay(500);
+                    progress.Report($"running! ThreadID: {Thread.CurrentThread.ManagedThreadId}\tNum: {j + 1}");
+
+                    await Task.Delay(500);
+                    progress.Report($"finished! ThreadID: {Thread.CurrentThread.ManagedThreadId}\tNum: {j + 1}");
+                });
+                tasks[i] = t;
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.ReadKey();
+        }
+    }
+}
